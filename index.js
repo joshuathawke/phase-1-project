9f5f0ed99cad522ea0de27ad872c21c2835ba319
Diana Jordan <dianacjordan@gmail.com>
2023-03-06 14:02:33 -0500

    adds spans for each spirit

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-document.addEventListener('click', () => {
-    function 
-}
-)
+document.addEventListener('DOMContentLoaded', init)
+
+function init () {
+const spirits = ['vodka', 'rum', 'tequila']
+
+spirits.forEach(renderSpirits)
+}//end init
+  
+
+function renderSpirits (spirit){
+ const bar = document.querySelector('#alcohol-bar')
+    const span = document.createElement('span')
+    span.innerText = spirit
+    span.addEventListener('click', () => { 
+     console.log(span.innerText)})
+
+bar.append(span)
+
+
+    }
+   
+    
+    
+  
+
+
+
+
+
+
+
+ 
+
+
+
+
