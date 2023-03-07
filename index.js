ddcb1376c3da6000cff3a8ec17d6a6e429627dbb
Diana Jordan <dianacjordan@gmail.com>
2023-03-07 17:23:04 -0500

    remove drink details when new search, ingredients as list

--- index.js
+++ index.js
@@ -15,23 +15,23 @@ function init() {
     // add a 'submit' Event Listener to form#search-drinks and call searchDrink funciton with it's value
     drinkForm.addEventListener('submit', (e) => {
         e.preventDefault()
-        
+
         searchDrink(drinkForm.drink.value)
         drinkForm.reset();
-        
+
     })
 
 }//end init
 
 
 // take value submited in form#search-drinks > create the url for the search  > fetch that search > call renderDrinkList on resp Objects
-function searchDrink( formValue) {
+function searchDrink(formValue) {
     formValue = formValue.trim()
     formValue = formValue.split(' ').join('_')
 
     // due to issues with the API when an empty submit is entered. 
     // checks if formValue is empty and if so changes value to null
-    if(formValue === ''){
+    if (formValue === '') {
         formValue = null
     }
 
@@ -42,7 +42,7 @@ function searchDrink( formValue) {
                 // clearing the drink list so the old results don't remain if a new search returns no results
                 const ul = document.querySelector('#list')
                 ul.innerHTML = ''
-
+                detailsInfo.innerHTML = ''
                 window.alert("Sorry, we didn't find any results matching this search.")
             } else {
                 renderDrinksList(data)
@@ -51,8 +51,8 @@ function searchDrink( formValue) {
 
 } //end searchDrink
 
+const detailsInfo = document.getElementById('cocktail-details')
 
-//// DIANA  & css grid
 function renderSpirits(spirit) {
     const bar = document.querySelector('#alcohol-bar')
     const span = document.createElement('span')
@@ -84,68 +84,59 @@ function mouseColor(item) {
 function renderDrinksList(drinksBySpirit) {
     const ul = document.querySelector('#list')
     ul.innerHTML = ''
-
+    detailsInfo.innerHTML = ''
     drinksBySpirit.drinks.forEach(drink => {
         const li = document.createElement('li')
         li.innerText = drink.strDrink
         ul.append(li)
 
-        li.addEventListener('click', (e) => {
+        li.addEventListener('click', () => {
             drinkDetails(drink.idDrink)
-
-        })
-        //end event listener
-
+        })        //end event listener
         mouseColor(li)
 
     })
 }  // end renderDrinksList
 
-
-////JOSHUA
 function drinkDetails(drink) {
 
     //run a fetch based on the ID and pull picture, ingredients, steps
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink}`)
         .then(resp => resp.json())
         .then(drinkDetails => {
-           
-            
-            const detailsInfo = document.getElementById('cocktail-details')
+
+            detailsInfo.innerHTML = ''
             const drinkImg = document.createElement('img')
             const drinkName = document.createElement('h2')
             const drinkIngredients = document.createElement('ul')
             const drinkInstructions = document.createElement('p')
             const drinkGlass = document.createElement('p')
+            const drinkGlassHeader = document.createElement('b')
 
-            let ingredients = "";
             for (let i = 1; i <= 15; i++) {
                 let ingredient = drinkDetails.drinks[0][`strIngredient${i}`];
+
+                //add measurement. handle null measurements e.g. id=11634
+                let qty = drinkDetails.drinks[0][`strMeasure${i}`];
+                let measure = qty !== null ? qty + ' ' : ''
+
                 if (ingredient) {
-                    if (i === 1) {
-                        ingredients += ingredient;
-                    } else {
-                        ingredients += ", " + ingredient;
-                    }
+                    const li = document.createElement('li')
+                    li.innerText = measure + ingredient
+                    drinkIngredients.appendChild(li)
+                    console.log(li)
                 } else {
                     break;
                 }
             }
-            
-            drinkGlass.innerText = drinkDetails.drinks[0].strGlass
+
+            drinkGlassHeader.innerText = 'Serve in: '
+            drinkGlass.append(drinkGlassHeader, drinkDetails.drinks[0].strGlass)
             drinkName.innerText = drinkDetails.drinks[0].strDrink
             drinkImg.src = drinkDetails.drinks[0].strDrinkThumb
             drinkInstructions.innerText = drinkDetails.drinks[0].strInstructions
-            drinkIngredients.innerText = ingredients;
-            
             detailsInfo.append(drinkName, drinkImg, drinkIngredients, drinkInstructions, drinkGlass)
-
-
-
         })
-    // render.drinkDetails()
-    //render in the div
-
 
 
 } //end drinkDetails
